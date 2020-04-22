e210f455942354959c739efb833ca025d425d0c5
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-04-21 20:34:02 -0400

    Move startup code to frame

--- src/scripts/PmqStart.lua
+++ src/scripts/PmqStart.lua
@@ -1,24 +1,6 @@
 local _, addon = ...
 addon:traceFile("PmqStart.lua")
 
-function PMQ_EventFrame_OnLoad(self)
-  if addon.events == nil then
-    error("Addon events did not load, check PmqEvents.lua")
-  end
-
-  -- Must explicitly declare addon.events as 'self' for the class method
-  addon:catch(addon.events.registerEventsToFrame, addon.events, self)
-end
-
-function PMQ_EventFrame_OnEvent(self, event, ...)
-  if addon.events == nil then
-    error("Addon events did not load, check PmqEvents.lua")
-  end
-
-  -- Must explicitly declare addon.events as 'self' for the class method
-  addon:catch(addon.events.onGameEvent, addon.events, event, ...)
-end
-
 addon.events:addOnLoadHandler(function()
   addon:info("PMQ Loaded")
 end)
