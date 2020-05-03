44b728a84aa893876dee77ec5fb3603f3bfc044d
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-05-03 00:51:36 -0400

    Translate demo quests to QuestScript

--- src/data/DemoQuestData.lua
+++ src/data/DemoQuestData.lua
@@ -4,51 +4,48 @@ addon:traceFile("DemoQuestData.lua")
 addon.DemoQuestData = {
   {
     id = "dkill-ally",
-    name = "Babby's First Quest",
-    level = 5,
-    objectives = {
-      "kill 2 u='Mangy Wolf'",
-      "kill 3 u=Chicken"
-    }
+    script = [[
+      quest "Babby's First Quest"
+      objective kill 2 t='Mangy Wolf'
+      objective kill 3 t=Chicken
+    ]]
   },
   {
     id = "dkill-horde",
-    name = "Grob Has a Queue",
-    level = 8,
-    objectives = {
-      "kill 3 u='Bloodtalon Scythemaw'",
-      "kill 3 u='Elder Mottled Boar'",
-    }
+    script = [[
+      quest name="Grob has a Queue"
+      obj kill 3 target='Bloodtalon Scythemaw'
+      obj kill 3 tar='Elder Mottled Boar'
+    ]]
   },
   {
     id = "demote-horde",
-    name = "Dancin' is my to do",
-    level = 1,
-    objectives = {
-      "EMOTE em=dance",
-      "EMOTE em=hug u=Chepi",
-      "EMOTE em=salute u='Officer Thunderstrider'",
-      "EMOTE em=roar u='Bluffwatcher' g=2",
-    }
+    script = [[
+      q n="Dancin' is my to do"
+      o EMOTE em=dance
+      o EMOTE em=hug t=Chepi
+      o EMOTE em=salute t='Officer Thunderstrider'
+      o EMOTE em=roar t='Bluffwatcher' g=2
+    ]]
   },
   {
     id = "dtalk-ally",
-    name = "Talkin' to the Squad",
-    objectives = {
-      "talkto u='Brog Hamfist'",
-      "talkto u='Innkeeper Farley'",
-      "talkto u='William Pestle'",
-      "talkto 3 u=\"Stormwind Guard\""
-    }
+    script = [[
+      quest name='Talkin\' to the Squad'
+      obj talkto t='Brog Hamfist'
+      obj talkto t='Innkeeper Farley'
+      obj talkto t='William Pestle'
+      obj talkto 3 t="Stormwind Guard"
+    ]]
   },
   {
     id = "dtalk-horde",
-    name = "Tbluff Getaway",
-    objectives = {
-      "TalkTo u=Bulrug",
-      "TalkTo u=\"Jyn Stonehoof\"",
-      "TalkTo u=Atepa",
-      "TalkTo 3 u=Bluffwatcher"
-    }
+    script = [[
+      q "Tbluff Getaway"
+      obj TalkTo t=Bulrug
+      obj TalkTo t="Jyn Stonehoof"
+      obj TalkTo t=Atepa
+      obj TalkTo 3 t=Bluffwatcher
+    ]]
   }
 }
