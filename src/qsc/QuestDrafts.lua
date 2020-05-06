3c3dbeb6c7791a37c4f45b421fdd8f670eadbddb
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-05-05 21:54:02 -0400

    Add more methods to QuestDrafts

--- src/qsc/QuestDrafts.lua
+++ src/qsc/QuestDrafts.lua
@@ -25,14 +25,18 @@ addon.QuestDraftStatus = {
 }
 local status = addon.QuestDraftStatus
 
+addon:OnSaveDataLoaded(function()
+  addon.QuestDrafts:Load()
+end)
+
 function addon.QuestDrafts:Save()
   addon.SaveData:Save("Drafts", drafts)
-  addon.AppEvents:Publish("DraftsSaved", drafts)
+  addon.AppEvents:Publish("DraftsSaved")
 end
 
 function addon.QuestDrafts:Load()
   drafts = addon.SaveData:LoadTable("Drafts")
-  addon.AppEvents:Publish("DraftsLoaded", drafts)
+  addon.AppEvents:Publish("DraftsLoaded")
 end
 
 function addon.QuestDrafts:NewDraft(name)
@@ -46,11 +50,75 @@ function addon.QuestDrafts:NewDraft(name)
     script = ""
   }
 
-  table.insert(drafts, draft)
+  drafts[draft.id] = draft
   self:Save()
+
+  draft = addon:CopyTable(draft)
+  addon.AppEvents:Publish("DraftCreated", draft.id, draft)
   return draft
 end
 
-addon:onload(function()
-  addon.QuestDrafts:Load()
-end)
+function addon.QuestDrafts:GetDraftByID(id)
+  local draft = drafts[id]
+  if draft == nil then return nil end
+  return addon:CopyTable(draft)
+end
+
+function addon.QuestDrafts:UpdateDraft(id, patch)
+  if type(id) == "table" then
+    -- If id was not specified, then use the id from the patch object
+    patch = id
+    id = patch.id
+  end
+
+  if id == nil then
+    addon:warn("Unable to update draft: no id was specified")
+    return nil
+  end
+  if patch == nil then
+    addon:warn("Unable to update draft: no patch object was specified")
+    return nil
+  end
+
+  local draft = drafts[id]
+  if draft == nil then
+    addon:warn("Unable to update draft: no draft exists with id", id)
+    return nil
+  end
+
+  local changed = false
+  if patch.name and draft.name ~= patch.name then
+    draft.name = patch.name
+    changed = true
+  end
+  if patch.version and draft.version ~= patch.version then
+    draft.version = patch.version
+    changed = true
+  end
+  if patch.status and draft.status ~= patch.status then
+    draft.status = patch.status
+    changed = true
+  end
+  if patch.script and draft.script ~= patch.script then
+    draft.script = patch.script
+    changed = true
+  end
+
+  draft = addon:CopyTable(draft)
+  if changed then
+    self:Save()
+    addon.AppEvents:Publish("DraftUpdated", id, draft)
+  end
+  return draft
+end
+
+function addon.QuestDrafts:DeleteDraft(id)
+  local draft = drafts[id]
+  if draft == nil then
+    return
+  end
+
+  drafts[id] = nil
+  self:Save()
+  addon.AppEvents:Publish("DraftDeleted", id)
+end
