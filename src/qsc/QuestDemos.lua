96afae34ba4aa5bd8020d607d593b6227cab630f
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-05-05 22:05:22 -0400

    Add methods to QuestDemos

--- src/qsc/QuestDemos.lua
+++ src/qsc/QuestDemos.lua
@@ -1,14 +1,36 @@
 local _, addon = ...
 addon:traceFile("QuestDemos.lua")
 
+--[[
+  Demo model:
+  {
+    id: "string",
+    script: "string"
+  }
+--]]
+
+local demos = {}
+
 addon.QuestDemos = {}
 
-function addon.QuestDemos:FindAll()
-  return addon.DemoQuestDB
+addon:onload(function()
+  for _, demo in pairs(addon.DemoQuestDB) do
+    demos[demo.id] = demo
+  end
+end)
+
+function addon.QuestDemos:GetDemos()
+  return addon:CopyTable(demos)
 end
 
-function addon.QuestDemos:FindByID(id)
-  for _, demo in pairs(addon.DemoQuestDB) do
-    if demo.id == id then return demo end
-  end
+function addon.QuestDemos:GetDemoByID(id)
+  return addon:CopyTable(demos[id])
+end
+
+function addon.QuestDemos:CopyToDrafts(id)
+  local demo = demos[id]
+  local draft = addon.QuestDrafts:NewDraft(id)
+  draft.script = demo.script
+  addon.QuestDrafts:UpdateDraft(draft)
+  return draft
 end
