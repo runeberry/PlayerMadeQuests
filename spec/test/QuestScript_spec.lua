a068812ba6ac5d1b91acbb70fba207fafb8ad89c
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-06-12 18:06:08 -0400

    More QuestScript tests

--- spec/test/QuestScript_spec.lua
+++ spec/test/QuestScript_spec.lua
@@ -11,15 +11,35 @@ local function compile_err(script, params)
   assert.has_error(function() compiler:Compile(script, params) end)
 end
 
-describe("QuestScript", function()
+describe("QuestScriptCompiler", function()
+  local tc, script
+
   setup(function()
     addon:Init()
   end)
+  before_each(function()
+    script = nil
+    tc = {}
+  end)
 
   it("can compile valid script", function()
-    compile([[objective kill something]])
+    script = [[objective kill something]]
+    compile(script)
   end)
   it("cannot compile invalid script", function()
-    compile_err([[bad juju]])
+    script = [[bad juju]]
+    compile_err(script)
+  end)
+  it("can recognize command aliases", function()
+    script = [[obj kill something
+    o talkto something]]
+    compile(script)
+  end)
+  it("can compile all demo quests", function()
+    -- todo: move this test to QuestDemos tests, if I ever make that
+    local demos = addon.QuestDemos:FindAll()
+    for _, demo in ipairs(demos) do
+      compile(demo.script, demo.parameters)
+    end
   end)
 end)
