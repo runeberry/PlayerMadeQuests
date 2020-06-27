0d8a06138f6514955e0d6cbac1a96553da6c15de
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-06-27 17:25:14 -0400

    Remove FAIAP

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -13,7 +13,6 @@ addon.AceGUI = LibStub("AceGUI-3.0")
 addon.LibCompress = LibStub("LibCompress")
 addon.LibScrollingTable = LibStub("ScrollingTable")
 addon.ParseYaml = LibStub("TinyYaml").parse
-addon.Indent = LibStub("IndentationLibYaml")
 
 -- WoW Global Functions
 addon.G = {
