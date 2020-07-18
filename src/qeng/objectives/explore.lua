006c169ed1b59c4b93498ae9d3d1219b21ceef8f
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-07-17 20:09:02 -0400

    Split location refresh from event publish on explore

--- src/qeng/objectives/explore.lua
+++ src/qeng/objectives/explore.lua
@@ -19,17 +19,21 @@ loader:AddScript(tokens.OBJ_EXPLORE, tokens.METHOD_POST_EVAL, function(obj, resu
 end)
 
 local function publish()
-  -- Calling GPL(true) here will ensure the data is refreshed for all conditions
-  addon.QuestEvents:Publish(tokens.OBJ_EXPLORE, addon:GetPlayerLocation(true))
+  addon.QuestEvents:Publish(tokens.OBJ_EXPLORE, addon:GetPlayerLocation())
+end
+
+local function refreshLocation()
+  addon:GetPlayerLocation(true)
 end
 
 addon.AppEvents:Subscribe("PlayerLocationChanged", publish)
-addon.AppEvents:Subscribe("QuestAdded", publish)
-addon.AppEvents:Subscribe("QuestLogBuilt", publish)
-addon.AppEvents:Subscribe("QuestTrackingStarted", publish)
-addon.GameEvents:Subscribe("ZONE_CHANGED", publish)
-addon.GameEvents:Subscribe("ZONE_CHANGED_INDOORS", publish)
-addon.GameEvents:Subscribe("ZONE_CHANGED_NEW_AREA", publish)
+
+addon.AppEvents:Subscribe("QuestAdded", refreshLocation)
+addon.AppEvents:Subscribe("QuestLogBuilt", refreshLocation)
+addon.AppEvents:Subscribe("QuestTrackingStarted", refreshLocation)
+addon.GameEvents:Subscribe("ZONE_CHANGED", refreshLocation)
+addon.GameEvents:Subscribe("ZONE_CHANGED_INDOORS", refreshLocation)
+addon.GameEvents:Subscribe("ZONE_CHANGED_NEW_AREA", refreshLocation)
 
 addon.AppEvents:Subscribe("ObjectiveCompleted", function(obj)
   addon:StopPollingLocation(obj.id)
