4b5e9b42bf8ebb1d520c56ae623971b7259ba505
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-07-27 22:51:25 -0400

    Add /pmq scan-events

--- src/events/GameEvents.lua
+++ src/events/GameEvents.lua
@@ -38,6 +38,29 @@ function addon.GameEvents:OnUnsubscribe(event)
   end
 end
 
+-- Helper function to subscribe to ALL known game events.
+-- Enable debug logs on PMQ:GameEvent to see what's being captured.
+local watchAllSubKeys
+function addon.GameEvents:ToggleWatchAll()
+  if watchAllSubKeys then
+    -- Events are already subscribed, unsubscribe from them
+    for event, key in pairs(watchAllSubKeys) do
+      self:Unsubscribe(event, key)
+    end
+    watchAllSubKeys = nil
+    addon.Logger:Warn("GameEvent scanning disabled.")
+  else
+    -- Events have not yet been subscribed, subscribe to them here
+    watchAllSubKeys = {}
+    local handler = function() end
+    for _, event in ipairs(addon.GameEventsList) do
+      -- Subscribe to the event and store the key to unsubscribe from it later
+      watchAllSubKeys[event] = self:Subscribe(event, handler)
+    end
+    addon.Logger:Warn("GameEvent scanning enabled. (%i events)", #addon.GameEventsList)
+  end
+end
+
 addon:onload(function()
   addon.GameEvents:Start()
 end)
