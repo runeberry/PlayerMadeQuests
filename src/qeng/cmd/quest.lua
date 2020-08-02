3cdf4dd1f0c7d719812aba6bd76ebfc3dbb2d95b
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-08-01 20:07:31 -0400

    Newline support for quest description/completion

--- src/qeng/cmd/quest.lua
+++ src/qeng/cmd/quest.lua
@@ -3,6 +3,11 @@ local loader = addon.QuestScriptLoader
 local compiler = addon.QuestScriptCompiler
 local tokens = addon.QuestScriptTokens
 
+-- todo: add wider support for this newline behavior
+local function subNewlines(str)
+  return str:gsub([[\n]], "\n"):gsub("%%br", "\n\n"):gsub("\n ", "\n")
+end
+
 loader:AddScript(tokens.CMD_QUEST, tokens.METHOD_PARSE, function(quest, args)
   local cmdInfo = addon.QuestScript[tokens.CMD_QUEST]
 
@@ -13,11 +18,11 @@ loader:AddScript(tokens.CMD_QUEST, tokens.METHOD_PARSE, function(quest, args)
 
   local description = compiler:GetValidatedParameterValue(tokens.PARAM_DESCRIPTION, args, cmdInfo)
   if description then
-    quest.description = description
+    quest.description = subNewlines(description)
   end
 
   local completion = compiler:GetValidatedParameterValue(tokens.PARAM_COMPLETION, args, cmdInfo)
   if completion then
-    quest.completion = completion
+    quest.completion = subNewlines(completion)
   end
 end)
