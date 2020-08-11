651fd2cb6d72c58b8aebdde2e54bb122f9e2ab28
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-08-10 20:14:03 -0400

    Refactor

--- src/gui/PositionFinderFrame.lua
+++ src/gui/PositionFinderFrame.lua
@@ -1,6 +1,7 @@
 local _, addon = ...
 local AceGUI = addon.AceGUI
 local QuestLog, QuestStatus, localizer = addon.QuestLog, addon.QuestStatus, addon.QuestScriptLocalizer
+local CreateFrame = addon.G.CreateFrame
 
 addon.PositionFinderFrame = nil -- Built at end of file
 
@@ -23,39 +24,51 @@ local function buildPositionFinderFrame()
   local frame = addon.CustomWidgets:CreateWidget("ToolWindowPopout", "PositionFinderFrame", frameOptions)
   local content = frame:GetContentFrame()
 
-  local scrollGroup = AceGUI:Create("SimpleGroup")
-  scrollGroup:SetFullWidth(true)
-  scrollGroup:SetFullHeight(true)
-  scrollGroup:SetLayout("Fill")
-
-  scrollGroup.frame:SetParent(content)
-  scrollGroup.frame:SetAllPoints(true)
-
-  local scroller = AceGUI:Create("ScrollFrame")
-  scroller:SetLayout("Flow")
-  scrollGroup:AddChild(scroller)
-
-  local buttonGroup = AceGUI:Create("SimpleGroup")
-  buttonGroup:SetFullWidth(true)
-  scroller:AddChild(buttonGroup)
-
-  local moreButton = AceGUI:Create("Button")
-  moreButton:SetText("Print Coordinates")
-  moreButton:SetWidth(135)
-  moreButton:SetCallback("OnClick", function()
+  local button = CreateFrame("Button", nil, content, "UIPanelButtonTemplate")
+  button:SetPoint("TOPLEFT", content, "TOPLEFT")
+  button:SetText("Print Coordinates")
+  button:SetWidth(135)
+  button:SetScript("OnClick", function()
     addon.Logger:Warn("Yay button!")
-    local GetPlayerMapPosition = addon.G.GetPlayerMapPosition
-    local GetBestMapForUnit = addon.G.GetBestMapForUnit
-    local map = GetBestMapForUnit("player")
-    local x, y = 0, 0
-    if map then
-      local position = GetPlayerMapPosition(map, "player")
-      x, y = position:GetXY()
-      addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", x * 100), string.format("%.2f", y * 100))
 
-    end
+    local location = addon:GetPlayerLocation()
+    addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", location.x), string.format("%.2f", location.y))
   end)
-  buttonGroup:AddChild(moreButton)
+
+
+  -- local scrollGroup = AceGUI:Create("SimpleGroup")
+  -- scrollGroup:SetFullWidth(true)
+  -- scrollGroup:SetFullHeight(true)
+  -- scrollGroup:SetLayout("Fill")
+
+  -- scrollGroup.frame:SetParent(content)
+  -- scrollGroup.frame:SetAllPoints(true)
+
+  -- local scroller = AceGUI:Create("ScrollFrame")
+  -- scroller:SetLayout("Flow")
+  -- scrollGroup:AddChild(scroller)
+
+  -- local buttonGroup = AceGUI:Create("SimpleGroup")
+  -- buttonGroup:SetFullWidth(true)
+  -- scroller:AddChild(buttonGroup)
+
+  -- local moreButton = AceGUI:Create("Button")
+  -- moreButton:SetText("Print Coordinates")
+  -- moreButton:SetWidth(135)
+  -- moreButton:SetCallback("OnClick", function()
+  --   addon.Logger:Warn("Yay button!")
+  --   local GetPlayerMapPosition = addon.G.GetPlayerMapPosition
+  --   local GetBestMapForUnit = addon.G.GetBestMapForUnit
+  --   local map = GetBestMapForUnit("player")
+  --   local x, y = 0, 0
+  --   if map then
+  --     local position = GetPlayerMapPosition(map, "player")
+  --     x, y = position:GetXY()
+  --     addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", x * 100), string.format("%.2f", y * 100))
+
+  --   end
+  -- end)
+  -- buttonGroup:AddChild(moreButton)
   return frame
 end
 
