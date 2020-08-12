4e3ccb316c4f52b0ca9a22993b7fc7d235f5def6
Ben Leahy <benleahy91@gmail.com>
2020-08-12 18:39:27 -0400

    added dynamic location text for location finder

--- src/gui/PositionFinderFrame.lua
+++ src/gui/PositionFinderFrame.lua
@@ -7,7 +7,7 @@ addon.PositionFinderFrame = nil -- Built at end of file
 
 local frameOptions = {
   styleOptions = {
-    text = "PMQ Position Finder"
+    text = "Location Finder"
   },
   position = {
     p1 = "RIGHT",
@@ -20,56 +20,41 @@ local frameOptions = {
   }
 }
 
+
 local function buildPositionFinderFrame()
-  local frame = addon.CustomWidgets:CreateWidget("ToolWindowPopout", "PositionFinderFrame", frameOptions)
+  local frame = addon.CustomWidgets:CreateWidget("ToolWindowPopout", "LocationFinderFrame", frameOptions)
+  frame:SetSize(400, 250)
+  frame:SetMinResize(400, 250)
   local content = frame:GetContentFrame()
+  local text = "PLAYER_POSITION"
+  local playerLocationText = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+  playerLocationText:SetText(text)
+  playerLocationText:SetPoint("TOPLEFT", content, "TOPLEFT")
+  playerLocationText:SetHeight(30)
 
-  local button = CreateFrame("Button", nil, content, "UIPanelButtonTemplate")
-  button:SetPoint("TOPLEFT", content, "TOPLEFT")
-  button:SetText("Print Coordinates")
-  button:SetWidth(135)
-  button:SetScript("OnClick", function()
-    addon.Logger:Warn("Yay button!")
-
+  addon:StartPollingLocation("location-frame")
+  addon.AppEvents:Subscribe("PlayerLocationChanged", function(loc)
     local location = addon:GetPlayerLocation()
-    addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", location.x), string.format("%.2f", location.y))
+    if location.subZone == "" then
+      text = location.zone .." ("..string.format("%.2f", location.x).. ", ".. string.format("%.2f", location.y)..")"
+    else
+      text = location.subZone.. ", "..location.zone .." ("..string.format("%.2f", location.x).. ", ".. string.format("%.2f", location.y)..")"
+    end
+
+    playerLocationText:SetText(text)
   end)
 
-
-  -- local scrollGroup = AceGUI:Create("SimpleGroup")
-  -- scrollGroup:SetFullWidth(true)
-  -- scrollGroup:SetFullHeight(true)
-  -- scrollGroup:SetLayout("Fill")
-
-  -- scrollGroup.frame:SetParent(content)
-  -- scrollGroup.frame:SetAllPoints(true)
-
-  -- local scroller = AceGUI:Create("ScrollFrame")
-  -- scroller:SetLayout("Flow")
-  -- scrollGroup:AddChild(scroller)
-
-  -- local buttonGroup = AceGUI:Create("SimpleGroup")
-  -- buttonGroup:SetFullWidth(true)
-  -- scroller:AddChild(buttonGroup)
-
-  -- local moreButton = AceGUI:Create("Button")
-  -- moreButton:SetText("Print Coordinates")
-  -- moreButton:SetWidth(135)
-  -- moreButton:SetCallback("OnClick", function()
-  --   addon.Logger:Warn("Yay button!")
-  --   local GetPlayerMapPosition = addon.G.GetPlayerMapPosition
-  --   local GetBestMapForUnit = addon.G.GetBestMapForUnit
-  --   local map = GetBestMapForUnit("player")
-  --   local x, y = 0, 0
-  --   if map then
-  --     local position = GetPlayerMapPosition(map, "player")
-  --     x, y = position:GetXY()
-  --     addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", x * 100), string.format("%.2f", y * 100))
-
-  --   end
-  -- end)
-  -- buttonGroup:AddChild(moreButton)
   return frame
 end
 
 addon.PositionFinderFrame = buildPositionFinderFrame()
+
+  -- local button = CreateFrame("Button", nil, content, "UIPanelButtonTemplate")
+  -- button:SetPoint("TOPLEFT", content, "TOPLEFT")
+  -- button:SetText("Print Coordinates")
+  -- button:SetWidth(135)
+  -- button:SetScript("OnClick", function()
+  --   addon.Logger:Warn("Yay button!")
+  --   local location = addon:GetPlayerLocation()
+  --   addon.Logger:Warn("Coordinates: %s, %s", string.format("%.2f", location.x), string.format("%.2f", location.y))
+  -- end)
