6133740cab47868a174da8ad3302c751f8ef4ba1
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-08-22 19:57:54 -0400

    Update event log format for consistency

--- src/events/Lifecycle.lua
+++ src/events/Lifecycle.lua
@@ -65,7 +65,7 @@ function addon.Lifecycle:OnSubscribe(event, handler)
     if handler.logLevel then
       logLevel = handler.logLevel
     end
-    self.logger:Log(logLevel, "Handling %s: %s", self.name, event)
+    self.logger:Log(logLevel, "Handling: %s", event)
     addon:catch(handler.fn)
     return false -- Do not continue with the subscription
   end
--- src/util/Events.lua
+++ src/util/Events.lua
@@ -20,7 +20,7 @@ local function handleEvent(broker, event, ...)
       -- If the handler has a log level, then override the broker's default log level
       logLevel = handler.logLevel
     end
-    broker.logger:Log(logLevel, "Handling %s: %s", broker.name, event)
+    broker.logger:Log(logLevel, "Handling: %s", event)
     addon:catch(handler.fn, ...)
   end
 end
@@ -37,7 +37,7 @@ local function processQueue(broker)
 end
 
 local function broker_Publish(self, event, ...)
-  self.logger:Debug("Publishing %s: %s", self.name, event)
+  self.logger:Debug("Publishing: %s", event)
   local handlers = self.handlersMap[event]
   if addon:tlen(handlers) == 0 then
     -- There are no handlers registered for this event
@@ -91,7 +91,7 @@ local function broker_Subscribe(self, event, handlerFunc, options)
 
   local key = addon:CreateID("subscription-%i")
   handlers[key] = handler
-  self.logger:Trace("Subscribed to %s: %s", self.name, event)
+  self.logger:Trace("Subscribed: %s", event)
   return key -- Key can be used to unsubscribe later
 end
 
@@ -120,7 +120,7 @@ local function broker_Unsubscribe(self, event, key)
     self.handlersMap[event] = nil
   end
 
-  self.logger:Trace("Unsubscribed from event: %s", event)
+  self.logger:Trace("Unsubscribed: %s", event)
   return true
 end
 
