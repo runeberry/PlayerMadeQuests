a0b4883dac310e2b9ed427b7b32da0f74fa8ed5c
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-08-26 23:42:07 -0400

    Fix colors for objective logger

--- src/qeng/QuestEngineLogger.lua
+++ src/qeng/QuestEngineLogger.lua
@@ -2,15 +2,23 @@ local _, addon = ...
 
 addon.QuestEngineLogger = addon.Logger:NewLogger("Objectives")
 
+local passPrefix = "[P] "
+local failPrefix = "[F] "
+
 local origLog = addon.QuestEngineLogger.Log
 addon.QuestEngineLogger.Log = function(self, loglevel, str, ...)
   origLog(self, loglevel, "    "..tostring(str), ...)
 end
 
 function addon.QuestEngineLogger:Pass(str, ...)
-  self:Debug(addon:Colorize("green", "[P] "..tostring(str)), ...)
+  self:Debug(passPrefix..tostring(str), ...)
 end
 
 function addon.QuestEngineLogger:Fail(str, ...)
-  self:Debug(addon:Colorize("red", "[F] "..tostring(str)), ...)
+  self:Debug(failPrefix..tostring(str), ...)
 end
+
+addon:OnConfigLoaded(function()
+  passPrefix = addon:Colorize("green", passPrefix)
+  failPrefix = addon:Colorize("red", failPrefix)
+end)
