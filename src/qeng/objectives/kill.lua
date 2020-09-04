f653f8e3e6c2b63401d03f8d13ab7f93f4cfb0cb
Ben Leahy <benleahy91@gmail.com>
2020-09-04 08:49:15 -0400

    added fix to pet kill bug

--- src/qeng/objectives/kill.lua
+++ src/qeng/objectives/kill.lua
@@ -1,10 +1,44 @@
 local _, addon = ...
 local logger = addon.QuestEngine.ObjectiveLogger
 local tokens = addon.QuestScriptTokens
+local GetUnitName = addon.G.GetUnitName
+
+
+local petDamageTable = {
+
+}
+local petName = GetUnitName("pet")
 
 addon:OnQuestEngineReady(function()
   addon.CombatLogEvents:Subscribe("PARTY_KILL", function(cl)
     addon.LastPartyKill = cl
+    petDamageTable[cl.sourceGuid] = nil
     addon.QuestEvents:Publish(tokens.OBJ_KILL, cl)
   end)
+  addon.CombatLogEvents:Subscribe("SWING_DAMAGE", function(cl)
+    if( cl.destName == petName )
+    then
+      petDamageTable[cl.sourceGuid] = true
+    end
+  end)
+  addon.CombatLogEvents:Subscribe("SPELL_CAST_SUCCESS", function(cl)
+    if( cl.destName == petName )
+    then
+      petDamageTable[cl.sourceGuid] = true
+    end
+  end)
+  addon.CombatLogEvents:Subscribe("UNIT_DIED", function(cl)
+    logger:Table(cl)
+    if(petDamageTable[cl.destGuid] == true)
+    then
+      logger:Trace(cl.destGuid)
+      addon.QuestEvents:Publish(tokens.OBJ_KILL, cl)
+    end
+    petDamageTable[cl.destGuid] = nil
+  end)
+  -- addon.GameEvents:Subscribe("COMBAT_LOG_EVENT_UNFILTERED", function(...)
+  --   logger:Table({ CombatLogGetCurrentEventInfo() })
+  --   local info = { CombatLogGetCurrentEventInfo() }
+  --   logger:Table(info)
+  -- end)
 end)
