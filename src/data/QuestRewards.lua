d50cafd0b9c5973824133172e9b227da0d6bf041
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-12-30 10:23:00 -0500

    Store quest rewards per reward instead of per quest

--- src/data/QuestRewards.lua
+++ src/data/QuestRewards.lua
@@ -1,43 +1,93 @@
 local _, addon = ...
 
-addon.QuestRewards = addon:NewRepository("Reward", "questId")
+
+addon.QuestRewards = addon:NewRepository("Reward", "rewardId")
 addon.QuestRewards:SetSaveDataSource("QuestRewards")
 addon.QuestRewards:EnableWrite(true)
 addon.QuestRewards:EnableCompression(true)
 addon.QuestRewards:EnableTimestamps(true)
 
-function addon.QuestRewards:SaveQuestRewards(quest, choice, claimed)
-  if not quest.rewards or not quest.rewards.parameters then return end
+local function buildRewardId(quest, identifier)
+  return string.format("reward-%s-%s", quest.questId, tostring(identifier))
+end
 
-  local players = {}
+local function getRewardGivers(quest)
+  local givers = {}
   for playerName in pairs(quest.rewards.parameters.player) do
     -- map from DistinctSet to simple array of names
-    players[#players+1] = playerName
+    givers[#givers+1] = playerName
   end
+  return givers
+end
 
-  local items
-  if quest.rewards.parameters.rewarditem then
-    items = addon:CopyTable(quest.rewards.parameters.rewarditem)
-  end
+local function buildItemReward(quest, itemId, quantity)
+  -- Assuming that the full info is available now
+  -- since it was displayed in the QuestInfoFrame
+  local item = addon:LookupItem(itemId)
 
-  local reward = {
+  return {
+    rewardId = buildRewardId(quest, itemId),
     questId = quest.questId,
-    questName = quest.name,
-    players = players,
-    items = items,
-    money = quest.rewards.parameters.rewardmoney,
-
-    choice = choice,
-    claimed = claimed or false,
+    givers = getRewardGivers(quest),
+    itemId = itemId,
+    itemName = item.name,
+    itemLink = item.link,
+    itemQuantity = quantity,
   }
+end
+
+local function buildMoneyReward(quest, money)
+  return {
+    rewardId = buildRewardId(quest, "money"),
+    questId = quest.questId,
+    givers = getRewardGivers(quest),
+    money = money,
+  }
+end
+
+local function saveReward(self, reward, force)
+  if not force then
+    -- Unless force is enabled, don't overwrite existing reward entries
+    if self:FindByID(reward) then return end
+  end
 
   self:Save(reward)
 end
 
-addon.AppEvents:Subscribe("QuestCompleted", function(quest)
-  if not addon.QuestRewards:FindByID(quest.questId) then
-    -- Save quest rewards, but don't overwrite existing rewards info
-    -- (in case the quest is being replayed)
-    addon.QuestRewards:SaveQuestRewards(quest)
+function addon.QuestRewards:SaveQuestRewards(quest, force)
+  if not quest.rewards or not quest.rewards.parameters or not quest.rewards.parameters.player then return end
+
+  local rewards = {}
+
+  if quest.rewards.parameters.rewarditem then
+    if quest.rewards.parameters.choose then
+      if quest.rewards.selectedIndex then
+        -- One item is rewarded as chosen by the player
+        local item = quest.rewards.parameters.rewarditem[quest.rewards.selectedIndex]
+        if item then
+          rewards[#rewards+1] = buildItemReward(quest, item.itemId, item.quantity)
+        end
+        -- Else an invalid selection was made (somehow?)
+      else
+        -- One item is rewarded, but no selection was made (somehow?)
+      end
+    else
+      -- All items are rewarded to the player
+      for _, item in ipairs(quest.rewards.parameters.rewarditem) do
+        rewards[#rewards+1] = buildItemReward(quest, item.itemId, item.quantity)
+      end
+    end
   end
+
+  if quest.rewards.parameters.rewardmoney and quest.rewards.parameters.rewardmoney > 0 then
+    rewards[#rewards+1] = buildMoneyReward(quest, quest.rewards.parameters.rewardmoney)
+  end
+
+  for _, reward in ipairs(rewards) do
+    saveReward(self, reward, force)
+  end
+end
+
+addon.AppEvents:Subscribe("QuestCompleted", function(quest)
+  addon.QuestRewards:SaveQuestRewards(quest)
 end)
--- src/gui/menu/QuestRewardsMenu.lua
+++ src/gui/menu/QuestRewardsMenu.lua
@@ -3,53 +3,83 @@ local QuestRewards = addon.QuestRewards
 local QuestLog, QuestArchive = addon.QuestLog, addon.QuestArchive
 local StaticPopups = addon.StaticPopups
 local CreateFrame = addon.G.CreateFrame
+local GetCoinTextureString = addon.G.GetCoinTextureString
 
-local questRewardRows = {}
+local rewardRows = {}
 
 local menu = addon.MainMenu:NewMenuScreen("QuestRewardsMenu")
 
 local options = {
   colInfo = {
     {
-      name = "Quest",
-      width = { flexSize = 2, min = 80 },
+      name = "Reward",
+      width = { flexSize = 3, min = 60 },
       align = "LEFT",
 
     },
     {
-      name = "Reward Giver",
-      width = { flexSize = 2, min = 40 },
-      align = "RIGHT",
+      name = "From",
+      width = { flexSize = 2, min = 60 },
+      align = "LEFT",
     },
     {
-      name = "Claimed?",
-      width = { flexSize = 1, min = 40 },
+      name = "Claimed",
+      width = 50,
       align = "CENTER"
     },
   },
   dataSource = function()
-    questRewardRows = {}
+    rewardRows = {}
     local rewards = QuestRewards:FindAll()
-    table.sort(rewards, function(a, b) return a.questId < b.questId end)
+    table.sort(rewards, function(a, b) return a.rewardId < b.rewardId end)
+
     for _, reward in ipairs(rewards) do
-      local players = ""
-      if #reward.players == 1 then
-        players = reward.players[1]
-      elseif #reward.players > 1 then
-        players = string.format("%s (+%i more)", reward.players[1], #reward.players - 1)
+      local rewardText = "???"
+      if reward.itemId then
+        rewardText = reward.itemLink or reward.itemName
+        if not rewardText then
+          -- If the item link/name are somehow unavailable, try looking up the item now
+          local item = addon:LookupItem(reward.itemId)
+          if item then
+            rewardText = item.link or item.name
+          end
+          if not rewardText then
+            rewardText = string.format("(Item #%i)", reward.itemId)
+          end
+        end
+
+        if reward.itemQuantity and reward.itemQuantity > 0 then
+          rewardText = string.format("%ix %s", reward.itemQuantity, rewardText)
+        end
+      elseif reward.money then
+        rewardText = GetCoinTextureString(reward.money)
       end
 
-      local status = ""
+      local givers = ""
+      if #reward.givers == 1 then
+        givers = reward.givers[1]
+      elseif #reward.givers > 1 then
+        givers = string.format("%s (+%i more)", reward.givers[1], #reward.givers - 1)
+      end
+
+      local claimed = ""
       if reward.claimed then
-        status = addon:Colorize("green", "Yes")
+        claimed = addon:Colorize("green", "Yes")
       else
-        status = addon:Colorize("red", "No")
+        claimed = addon:Colorize("red", "No")
       end
 
-      local row = { reward.questName, players, status, reward.questId }
-      questRewardRows[#questRewardRows+1] = row
+      local row = {
+        rewardText,
+        givers,
+        claimed,
+        -- Hidden cols
+        reward.rewardId,
+      }
+
+      rewardRows[#rewardRows+1] = row
     end
-    return questRewardRows
+    return rewardRows
   end,
   buttons = {
     {
@@ -121,7 +151,7 @@ function menu:Create(frame)
       },
       {
         style = "default",
-        text = "When you complete a quest that offers rewards, that quest will be shown here. "..
+        text = "When you complete a quest that offers money or item rewards, they will be shown here. "..
                "Message the players listed below to find out how to claim your rewards.",
       }
     }
