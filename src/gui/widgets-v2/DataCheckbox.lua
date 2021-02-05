c42f6d4d40a3f5798d918dc613fc3a85185d6076
dolphinspired <ultima749@gmail.com>
2021-02-04 20:43:15 -0500

    Add autoLoad option to DataCheckbox

--- src/gui/widgets-v2/DataCheckbox.lua
+++ src/gui/widgets-v2/DataCheckbox.lua
@@ -7,23 +7,28 @@ local widget = addon:NewWidget("DataCheckbox")
 local defaultOptions = {
   text = "",                -- [string] Text to display to the right of the checkbox
   spacing = 2,              -- [number] Spacing between the checkbox and its text
+
+  autoLoad = true,          -- [boolean] If true, the widget will be refreshed immediately
   get = nil,                -- [function() -> boolean] Getter to load the value
   set = nil,                -- [function(boolean)] Setter to save the value
 }
 
 local methods = {
   ["Refresh"] = function(self)
-    local value = self._options.get() and true
-    self:SetChecked(value)
+    self:SetChecked(self:GetValue())
+  end,
+  ["GetValue"] = function(self)
+    return self._options.get() and true -- coerce value to boolean
+  end,
+  ["SetValue"] = function(self, value)
+    value = value and true -- coerce value to boolean
+    self._options.set(value)
   end,
 }
 
 local scripts = {
   ["OnClick"] = function(self, mouseButton, isDown)
-    self._options.set(self:GetChecked())
-  end,
-  ["OnShow"] = function(self)
-    self:Refresh()
+    self:SetValue(self:GetChecked())
   end,
 }
 
@@ -46,6 +51,10 @@ function widget:Create(frameName, parent, options)
 
   addon:ApplyMethods(button, methods)
   addon:ApplyScripts(button, scripts)
+
+  if options.autoLoad then
+    button:Refresh()
+  end
 
   return button
 end
