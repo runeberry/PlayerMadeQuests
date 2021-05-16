e2a80fa8aacd3c1bbb0dda3b6978041fe6da2da6
dolphinspired <ultima749@gmail.com>
2021-05-16 00:50:37 -0400

    Add radio option to FormCheckbox

--- src/gui/widgets-v2/FormCheckbox.lua
+++ src/gui/widgets-v2/FormCheckbox.lua
@@ -6,6 +6,7 @@ local template = addon:NewFrame("FormCheckbox")
 local defaultOptions = {
   label = "",               -- [string] Text to display to the right of the checkbox
   spacing = 2,              -- [number] Spacing between the checkbox and its text
+  radio = false,            -- [boolean] Should the button look and act like a radio button?
 
   autoLoad = true,          -- [boolean] If true, the widget will be refreshed immediately
 }
@@ -18,7 +19,16 @@ template:AddMethods({
 
 template:AddScripts({
   ["OnClick"] = function(self, mouseButton, isDown)
-    self:SetFormValue(self:GetChecked())
+    local prevChecked = self:GetFormValue()
+    local curChecked = self:GetChecked()
+
+    if prevChecked and not curChecked and self._options.radio then
+      -- Clicking on a "checked" radio button should not "uncheck" the box
+      self:SetChecked(true)
+      return
+    end
+
+    self:SetFormValue(curChecked)
   end,
   ["OnFormValueChange"] = function(self, value, isUserInput)
     if isUserInput then return end
@@ -33,7 +43,10 @@ template:AddScripts({
 function template:Create(frameName, parent, options)
   options = addon:MergeOptionsTable(defaultOptions, options)
 
-  local button = addon:CreateFrame("CheckButton", frameName, parent, "UICheckButtonTemplate")
+  local templateName = "UICheckButtonTemplate"
+  if options.radio then templateName = "UIRadioButtonTemplate" end
+
+  local button = addon:CreateFrame("CheckButton", frameName, parent, templateName)
   button:SetText(options.label)
   button:SetPushedTextOffset(0, 0)
   button:SetNormalFontObject("GameFontNormal")
