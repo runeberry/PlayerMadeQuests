84fba0c375ea018fb08c6d6f468a179dcd7a08c7
dolphinspired <ultima749@gmail.com>
2021-05-19 17:11:08 -0400

    Escape path in docker-run.sh

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,1 +1,2 @@
-docker run --name pmq -v $(pwd):/addon --rm -it dolphinspired/wow-addon-container
+# See here for why this is the way it is: https://stackoverflow.com/a/50608818/7071436
+docker run --name pmq -v /$(pwd):/addon --rm -it dolphinspired/wow-addon-container
