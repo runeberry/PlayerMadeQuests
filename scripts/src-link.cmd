671a67fc38531428c397d4a918ff3ca1055fb5c5
dolphinspired <ultima749@gmail.com>
2021-05-19 16:57:44 -0400

    Update src-link for TBC

--- scripts/src-link.cmd
+++ scripts/src-link.cmd
@@ -1,4 +1,7 @@
 :: Run the following in an elevated command prompt
 
+cd "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns"
+mklink /d PlayerMadeQuests "D:\Repos\PlayerMadeQuests\src"
+
 cd "C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns"
 mklink /d PlayerMadeQuests "D:\Repos\PlayerMadeQuests\src"
