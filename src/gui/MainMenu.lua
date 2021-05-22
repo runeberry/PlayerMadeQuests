5b0c559ad5023088d5f35a70e1cc8b3b31024d94
dolphinspired <ultima749@gmail.com>
2021-05-22 15:08:41 -0400

    Catch errors on MainMenu creation

--- src/gui/MainMenu.lua
+++ src/gui/MainMenu.lua
@@ -4,45 +4,52 @@ local UISpecialFrames = addon.G.UISpecialFrames
 local firstShow = true
 local defaultMenu = "AboutMenu"
 
-addon.MainMenu = addon.CustomWidgets:CreateWidget("TreeMenuFrame")
-addon.MainMenu:SetTitle("PlayerMadeQuests")
-addon.MainMenu:SetStatusText("PMQ "..addon:GetVersionText().." (thank you for testing! <3)")
-addon.MainMenu:SetMenuTree({ -- value == menuId
-  { value = "AboutMenu", text = "About PMQ" },
-  { value = "QuestMenu-Placeholder", text = "Quests",
-    children = {
-      { value = "QuestCatalogMenu", text = "Quest Catalog" },
-      { value = "QuestDemoListMenu", text = "Demo Quests" },
-      { value = "QuestDraftListMenu", text = "My Drafts" },
-      { value = "QuestLogMenu", text = "My Quest Log" },
-      { value = "QuestRewardsMenu", text = "Quest Rewards" },
-      { value = "QuestArchiveMenu", text = "Quest Archive" },
-    }
-  },
-  { value = "SettingsMenu", text = "Settings",
-    children = {
-      { value = "ConfigMenu", text = "Configuration" },
-      { value = "LoggingMenu", text = "Logging" },
-    }
-  },
-  { value = "HelpMenu-Placeholder", text = "Help",
-    children = {
-      { value = "HelpCommandsMenu", text = "Commands" },
-      { value = "HelpFaqMenu", text = "FAQs" },
-    }
-  },
-})
-addon.MainMenu:SetScript("OnShow", function()
-  if firstShow then
-    addon.MainMenu:NavToMenuScreen(defaultMenu)
-    firstShow = false
-  end
-end)
+local function initMainMenu()
+  addon.MainMenu = addon.CustomWidgets:CreateWidget("TreeMenuFrame")
+  addon.MainMenu:SetTitle("PlayerMadeQuests")
+  addon.MainMenu:SetStatusText("PMQ "..addon:GetVersionText().." (thank you for testing! <3)")
+  addon.MainMenu:SetMenuTree({ -- value == menuId
+    { value = "AboutMenu", text = "About PMQ" },
+    { value = "QuestMenu-Placeholder", text = "Quests",
+      children = {
+        { value = "QuestCatalogMenu", text = "Quest Catalog" },
+        { value = "QuestDemoListMenu", text = "Demo Quests" },
+        { value = "QuestDraftListMenu", text = "My Drafts" },
+        { value = "QuestLogMenu", text = "My Quest Log" },
+        { value = "QuestRewardsMenu", text = "Quest Rewards" },
+        { value = "QuestArchiveMenu", text = "Quest Archive" },
+      }
+    },
+    { value = "SettingsMenu", text = "Settings",
+      children = {
+        { value = "ConfigMenu", text = "Configuration" },
+        { value = "LoggingMenu", text = "Logging" },
+      }
+    },
+    { value = "HelpMenu-Placeholder", text = "Help",
+      children = {
+        { value = "HelpCommandsMenu", text = "Commands" },
+        { value = "HelpFaqMenu", text = "FAQs" },
+      }
+    },
+  })
+  addon.MainMenu:SetScript("OnShow", function()
+    if firstShow then
+      addon.MainMenu:NavToMenuScreen(defaultMenu)
+      firstShow = false
+    end
+  end)
 
--- Make closable with ESC
-local mmfGlobalName = addon:CreateGlobalName("MainMenuFrame")
-_G[mmfGlobalName] = addon.MainMenu
-table.insert(UISpecialFrames, mmfGlobalName)
+  -- Make closable with ESC
+  local mmfGlobalName = addon:CreateGlobalName("MainMenuFrame")
+  _G[mmfGlobalName] = addon.MainMenu
+  table.insert(UISpecialFrames, mmfGlobalName)
+end
+
+-- todo: move this to lifecycle, if I can figure out the timing...
+addon:catch(function()
+  initMainMenu()
+end)
 
 addon:OnGuiReady(function()
   addon.MainMenu:SetVisibleTreeDepth(2)
