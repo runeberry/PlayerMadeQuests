7324268363c5f794bb7a716294831c096cb8a6d4
dolphinspired <ultima749@gmail.com>
2021-06-27 15:20:29 -0400

    Remove side resizers for MixinResizable

--- src/gui/widgets-v2/MixinResizable.lua
+++ src/gui/widgets-v2/MixinResizable.lua
@@ -21,35 +21,25 @@ template:AddMethods({
   end,
 })
 
-local function sizerseOnMouseDown(self)
-  self:GetParent():StartSizing("BOTTOMRIGHT")
-end
-
-local function sizersOnMouseDown(self)
-  self:GetParent():StartSizing("BOTTOM")
-end
-
-local function sizereOnMouseDown(self)
-  self:GetParent():StartSizing("RIGHT")
-end
-
-local function sizerOnMouseUp(self)
-  self:GetParent():StopMovingOrSizing()
-end
+local resizerScripts = {
+  ["OnMouseDown"] = function(self)
+    self:GetParent():StartSizing("BOTTOMRIGHT")
+  end,
+  ["OnMouseUp"] = function(self)
+    self:GetParent():StopMovingOrSizing()
+  end,
+}
 
 --- Adapted from Ace3: AceGUIContainer-Window.lua (see license information below)
 local function addSizerWidget(frame)
-  local sizer_se = CreateFrame("Frame",nil,frame)
-  sizer_se:SetPoint("BOTTOMRIGHT",frame,"BOTTOMRIGHT",0,0)
-  sizer_se:SetWidth(25)
-  sizer_se:SetHeight(25)
-  sizer_se:EnableMouse()
-  sizer_se:SetScript("OnMouseDown",sizerseOnMouseDown)
-  sizer_se:SetScript("OnMouseUp", sizerOnMouseUp)
-  -- self.sizer_se = sizer_se
+  local sizer = addon:CreateFrame("Frame", "Resizer%i", frame)
+  sizer:SetPoint("BOTTOMRIGHT",frame,"BOTTOMRIGHT",0,0)
+  sizer:SetWidth(25)
+  sizer:SetHeight(25)
+  sizer:EnableMouse()
+  addon:ApplyScripts(sizer, resizerScripts)
 
-  local line1 = sizer_se:CreateTexture(nil, "BACKGROUND")
-  -- self.line1 = line1
+  local line1 = sizer:CreateTexture(nil, "BACKGROUND")
   line1:SetWidth(14)
   line1:SetHeight(14)
   line1:SetPoint("BOTTOMRIGHT", -8, 8)
@@ -57,32 +47,13 @@ local function addSizerWidget(frame)
   local x = 0.1 * 14/17
   line1:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5)
 
-  local line2 = sizer_se:CreateTexture(nil, "BACKGROUND")
-  -- self.line2 = line2
+  local line2 = sizer:CreateTexture(nil, "BACKGROUND")
   line2:SetWidth(8)
   line2:SetHeight(8)
   line2:SetPoint("BOTTOMRIGHT", -8, 8)
   line2:SetTexture(137057) -- Interface\\Tooltips\\UI-Tooltip-Border
   x = 0.1 * 8/17
   line2:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5)
-
-  local sizer_s = addon:CreateFrame("Frame",nil,frame)
-  sizer_s:SetPoint("BOTTOMRIGHT",frame,"BOTTOMRIGHT",-25,0)
-  sizer_s:SetPoint("BOTTOMLEFT",frame,"BOTTOMLEFT",0,0)
-  sizer_s:SetHeight(25)
-  sizer_s:EnableMouse()
-  sizer_s:SetScript("OnMouseDown",sizersOnMouseDown)
-  sizer_s:SetScript("OnMouseUp", sizerOnMouseUp)
-  -- self.sizer_s = sizer_s
-
-  local sizer_e = addon:CreateFrame("Frame",nil,frame)
-  sizer_e:SetPoint("BOTTOMRIGHT",frame,"BOTTOMRIGHT",0,25)
-  sizer_e:SetPoint("TOPRIGHT",frame,"TOPRIGHT",0,0)
-  sizer_e:SetWidth(25)
-  sizer_e:EnableMouse()
-  sizer_e:SetScript("OnMouseDown",sizereOnMouseDown)
-  sizer_e:SetScript("OnMouseUp", sizerOnMouseUp)
-  -- self.sizer_e = sizer_e
 end
 
 function template:Create(frame, options)
